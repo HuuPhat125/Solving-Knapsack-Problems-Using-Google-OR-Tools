85ae306d05c55c1dfd77d3569f00cf6a84f9cd89
huuphat <phatd142@gmail.com>
2024-04-05 08:35:28 +0700

    fix main

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def read_file(file_path):
     return [capacities], values, [weights]
 
 def knapsack(capacities, values, weights, time_limit):
-    start_time = time.time()
     # Create the solver.
     solver = knapsack_solver.KnapsackSolver(
         knapsack_solver.SolverType.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
@@ -25,8 +24,13 @@ def knapsack(capacities, values, weights, time_limit):
     # đặt giới hạn thời gian để kết thúc chương trình và trả về lời giải tốt nhất đã tìm thấy tại thời điểm này
     solver.set_time_limit(time_limit)
 
+    start_time = time.time()
+
     solver.init(values, weights, capacities)
+
     computed_value = solver.solve()
+
+    time_excution = time.time() - start_time
 
     packed_items = []
     packed_weights = []
@@ -40,14 +44,20 @@ def knapsack(capacities, values, weights, time_limit):
     print("Total weight:", total_weight)
     print("Packed items:", packed_items)
     print("Packed_weights:", packed_weights)
-    time_excution = time.time() - start_time
+    
     print("time excution", time_excution)
     return computed_value, total_weight, packed_items, packed_weights, time_excution
 def main():
+
+    parser = argparse.ArgumentParser(description='Solve knapsack problem with time limit')
+    parser.add_argument('time_limit', type=int, help='Time limit in seconds')
+    args = parser.parse_args()
+    time_limit = args.time_limit
+
     dir_path = os.getcwd()
     dir_path = os.path.join(dir_path, 'kplib')
     # tạo thư mục chứa output
-    output_dir = os.path.join(os.getcwd(), 'output')
+    output_dir = os.path.join(os.getcwd(), f'output_{time_limit}')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -67,24 +77,24 @@ def main():
                 print(s_path)
                 capacities, values, weights = read_file(s_path)
 
-                parser = argparse.ArgumentParser(description='Solve knapsack problem with time limit')
-                parser.add_argument('time_limit', type=int, help='Time limit in seconds')
-                args = parser.parse_args()
-                time_limit = args.time_limit
                 computed_value, total_weight, packed_items, packed_weights, time_excution = knapsack(capacities, values, weights, time_limit)
 
                 output_file_name = f'{i}_{case}_{r}_{kp_filename}.txt'
                 output_path = os.path.join(output_dir, output_file_name)
 
-                f = open(output_path,"x")
-                f.write('Test case name: ' + output_file_name + '\n')
-                f.write('time knapsack caculate: '+str(time_excution)+'\n')
-                f.write('Total value = '+ str(computed_value)+'\n')
-                f.write('Total weight: '+ str(total_weight)+'\n')
-                f.write('Packed items: '+ str(packed_items)+'\n')
-                f.write('packed_weights: '+str (packed_weights))
-
-    
+                # kiểm tra tính optimal: 
+                # giả sử: nếu thời gian thực thi nhỏ hơn time_limit thì lời giải optimal
+                optimal = time_excution < time_limit
+                
+                with open(output_path, "w") as f:
+                    f.write('Test case name: ' + output_file_name + '\n')
+                    f.write('time knapsack caculate: '+str(time_excution)+'\n')
+                    f.write('Total value = '+ str(computed_value)+'\n')
+                    f.write('Total weight: '+ str(total_weight)+'\n')
+                    f.write('Packed items: '+ str(packed_items)+'\n')
+                    f.write('packed_weights: '+str (packed_weights))
+                with open(os.path.join(os.getcwd(), f'output_{time_limit}.txt'), 'a') as file:
+                    file.write(f'{output_file_name}, {computed_value}, {total_weight}, {time_excution}, {optimal}\n')
 
 if __name__ == "__main__":
     main()
